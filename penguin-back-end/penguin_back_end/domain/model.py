63916a7f3afc84e8e447c4f6393fc1bf95f4dd05
Fabian Schomerus <97347781+fabayoun@users.noreply.github.com>
2022-12-04 15:58:49 +0000

    update grammar

--- penguin-back-end/penguin_back_end/domain/model.py
+++ penguin-back-end/penguin_back_end/domain/model.py
@@ -11,7 +11,7 @@ class Penguin:
     sentence: str = None
 
     def think_of_sentence(self):
-        if len(self.name_of_friend):
+        if len(self.name_of_friend) == 0:
             self.name_of_friend = "Your Penguin Mum"
         generator = SentenceGenerator(self.name_of_friend)
         sentence = generator.choose_sentence()
@@ -20,7 +20,7 @@ class Penguin:
     def add_suggestion(self):
         response = requests.get("https://www.boredapi.com/api/activity")
         response_dict = json.loads(response.text)
-        activity = response_dict['activity'].lower().replace(" you ", f" {self.name_of_friend} ").replace(" your ", f" {self.name_of_friend}'s ").replace(" have ", " has ").replace(" haven't ", " hasn't ").replace(" don't ", " doesn't ")
+        activity = response_dict['activity'].lower().replace(" you ", f" {self.name_of_friend} ").replace(" your ", f" their ").replace(" have ", " has ").replace(" haven't ", " hasn't ").replace(" don't ", " doesn't ").replace(" you're ", " they're ")
         return f" Maybe {self.name_of_friend} should {activity}"
 
     def say_sentence(self) -> str:
