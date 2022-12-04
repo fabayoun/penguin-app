3b6bf475c8a675b9ef16f5c3c08544735cf9462e
Fabian Schomerus <97347781+fabayoun@users.noreply.github.com>
2022-12-04 12:46:39 +0000

    add penguin methods

--- penguin-back-end/penguin_back_end/domain/model.py
+++ penguin-back-end/penguin_back_end/domain/model.py
@@ -1,10 +1,23 @@
+import json
+
 import attr
+from penguin_back_end.random_sentence_generator.sentence_generator import SentenceGenerator
 
 
 @attr.s
 class Penguin:
-    name: str
-    sentence: str
+    name_of_friend: str
+    sentence: str = None
+
+    def think_of_sentence(self):
+        generator = SentenceGenerator(self.name_of_friend)
+        self.sentence = generator.choose_sentence()
+
+    def say_sentence(self) -> str:
+        content = {"sentence": self.sentence}
+        json_string = json.dumps(content)
+        return json_string
+
 
 
 
--- penguin-back-end/penguin_back_end/main.py
+++ penguin-back-end/penguin_back_end/main.py
@@ -1,10 +1,17 @@
+from penguin_back_end.domain.model import Penguin
 
 
 def main():
     # run and listen to api
     # when receiving input
         # create penguin class
-        # add input
+        name = "Dave"
+        penguin = Penguin(name=name)
+
+        penguin.think_of_sentence()
+        sentence = penguin.say_sentence()
+
+
         # call random sentence generator
     # return json file
 
